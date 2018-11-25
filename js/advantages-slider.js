fe911e22b8e00f83f3e8016326a117cb3d694d6f
RomanSyakaev <vippy1@mail.ru>
2018-11-25 19:30:58 +0400

    update

--- js/advantages-slider.js
+++ js/advantages-slider.js
@@ -1,9 +1,9 @@
 ;{"use strict";
-let prev = document.querySelector(".reviews__prev");
-let next = document.querySelector(".reviews__next");
-let slide1 = document.querySelector("#reviews-input-1");
-let slide2 = document.querySelector("#reviews-input-2");
-let slide3 = document.querySelector("#reviews-input-3");
+let prev = document.querySelector(".advantages__btn-left");
+let next = document.querySelector(".advantages__btn-right");
+let slide1 = document.querySelector("#advantages-input-1");
+let slide2 = document.querySelector("#advantages-input-2");
+let slide3 = document.querySelector("#advantages-input-3");
 let slides = [slide1, slide2, slide3];
 let checkedSlide = slide1;
 
